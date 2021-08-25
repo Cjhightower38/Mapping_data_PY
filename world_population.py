3917c16686b950fc2e9331dad784355dd71cec0a
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-25 17:20:09 -0400

    original commit

---
+++ world_population.py
@@ -0,0 +1,21 @@
+# Importing json module to with with the info from the file.
+import json
+
+'''
+Store the downloaded json file in a variable. Open and rename the file
+as (f) then create a different variable to store the file using the 
+json.load(). Now itterate through each iteam in the variable or json 
+list inside a dictonary and if the key is equal to 2010 the name and 
+population are printed to screen.
+'''
+filename = 'population_data.json'
+with open(filename) as f:
+	pop_data = json.load(f)
+	
+#Print the 2010 population for each country.
+for pop_dict in pop_data:
+	if pop_dict['Year'] == '2010':
+		country_name = pop_dict['Country Name']
+		population = pop_dict['Value']
+		print(country_name + ': ' + population)
+	
