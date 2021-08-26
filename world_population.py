5bcfd8842337baa3787ef32d3b56e0a7b525ab74
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-25 20:18:48 -0400

    convert string into float then into integer(sloving ValueError due to decimal)

--- world_population.py
+++ world_population.py
@@ -13,10 +13,12 @@ filename = 'population_data.json'
 with open(filename) as f:
 	pop_data = json.load(f)
 	
-#Print the 2010 population for each country.
+#Because some populations have decimal values by converting the string
+#pop_dict['Value'] into a float and then a integer will correct the 
+#ValueError.
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
-		population = int(pop_dict['Value'])
+		population = int(float(pop_dict['Value']))
 		print(country_name + ': ' + str(population))
 	
