d06dc871b84b97603706360240280a6c1752267b
Cjhightower38 <Cjhightower38@gmail.com>
2021-08-25 20:10:40 -0400

    convert stings into numerical values

--- world_population.py
+++ world_population.py
@@ -6,7 +6,8 @@ Store the downloaded json file in a variable. Open and rename the file
 as (f) then create a different variable to store the file using the 
 json.load(). Now itterate through each iteam in the variable or json 
 list inside a dictonary and if the key is equal to 2010 the name and 
-population are printed to screen.
+population are printed to screen. Next covert the srings into numerical
+values by using the int() and the srt().
 '''
 filename = 'population_data.json'
 with open(filename) as f:
@@ -16,6 +17,6 @@ with open(filename) as f:
 for pop_dict in pop_data:
 	if pop_dict['Year'] == '2010':
 		country_name = pop_dict['Country Name']
-		population = pop_dict['Value']
-		print(country_name + ': ' + population)
+		population = int(pop_dict['Value'])
+		print(country_name + ': ' + str(population))
 	
